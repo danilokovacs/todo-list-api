222db22b2d1dfd349e8a66d3968527bbf32f64f1
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-03-22 14:41:54 -0300

    Spring Template

---
+++ src/test/kotlin/com/kovacs/todolistapi/TodoListApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kovacs.todolistapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
