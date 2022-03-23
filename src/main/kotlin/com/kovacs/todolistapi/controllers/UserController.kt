9bdc28a539757350660abf71669e98ede443ca56
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-03-23 09:04:15 -0300

    feat: POST create user endpoint

--- src/main/kotlin/com/kovacs/todolistapi/controllers/UserController.kt
+++ src/main/kotlin/com/kovacs/todolistapi/controllers/UserController.kt
@@ -1,5 +1,7 @@
 package com.kovacs.todolistapi.controllers
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.kovacs.todolistapi.models.User
 import com.kovacs.todolistapi.repositories.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,4 +17,12 @@ class UserController (private val userRepository: UserRepository) {
         return ResponseEntity.ok(users)
     }
 
+    @PostMapping("/user/create")
+    fun postUser(
+        @RequestBody json: String
+    ): ResponseEntity<Any> {
+        val user = userRepository.save(jacksonObjectMapper().readValue(json, User::class.java))
+
+        return ResponseEntity.ok(json)
+    }
 }
